b5e1ba879baa6726f30ad0ad591eb740b9d94143
Boliboe <iamjohnsomto@gmail.com>
2023-08-01 20:42:11 +0100

    functions added for the calendar month/year

--- assets/js/sanbox.js
+++ assets/js/sanbox.js
@@ -14,3 +14,32 @@ getFebDays = (year) => {
     return isLeapYear(year) ? 29 : 28
 }
 
+let calendar = document.querySelector('calendar')
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+'July', 'August', 'September', 'October', 'November', 'December']
+
+let monthPicker = document.querySelector('#month-picker')
+
+// GENERATE CALENDAR
+generateCalendar = (month, year) => {
+    let calendardays = document.querySelector('.calendar-days')
+    let calendarHeaderYear = document.querySelector('#year')
+
+    let daysOfMonth = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30,
+        31]
+
+        let currData = new Date()
+
+        monthPicker.innerHTML = monthNames[month]
+        calendarHeaderYear.innerHTML = year 
+
+        let firstDay = new Date(month, year, 1)
+
+        for (let i = 0; i<= daysOfMonth[month] + firs .getDay() -1; i++) {
+            
+        }
+
+}
+
+
